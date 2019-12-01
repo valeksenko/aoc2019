12acea04840e721de56a22b24d715cd40788603a
Val Aleksenko <valeriy.aleksenko@getbraintree.com>
2019-12-01 07:57:20 -0500

    D01P1

---
+++ README.md
@@ -0,0 +1,7 @@
+# aoc2019
+
+Advent of Code 2019
+
+http://adventofcode.com/2019
+
+run: `stack test`
---
+++ test/Spec.hs
@@ -0,0 +1,11 @@
+import Test.Tasty
+import qualified D01P1Spec
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests"
+    [
+        D01P1Spec.tests
+    ]
