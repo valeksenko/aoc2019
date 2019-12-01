f52fb6d2f3962b521b013976ae89df8bd0e451b5
Val Aleksenko <valeriy.aleksenko@getbraintree.com>
2019-12-01 08:37:52 -0500

    D01P2

---
+++ test/D01P2Spec.hs
@@ -0,0 +1,20 @@
+module D01P2Spec (
+    tests
+) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import D01P2
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+    [
+        testCase "gives correct answer to the original problem" $ do
+            2 @=? fullfuelrequirements 14
+            966 @=? fullfuelrequirements 1969
+            50346 @=? fullfuelrequirements 100756
+    ]
